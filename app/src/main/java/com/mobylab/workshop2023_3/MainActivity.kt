36e71bbfc1d985c92cc7e47c9b66d3e46af6763e
Radu-Ioan Ciobanu <radu.ciobanu@upb.ro>
2023-07-26 16:24:44 +0300

    Added tip calculation function

--- app/src/main/java/com/mobylab/workshop2023_3/MainActivity.kt
+++ app/src/main/java/com/mobylab/workshop2023_3/MainActivity.kt
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.mobylab.workshop2023_3.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +18,28 @@ class MainActivity : AppCompatActivity() {
 
         binding.calculateButton.setOnClickListener {
             Toast.makeText(this, "Button tapped", Toast.LENGTH_LONG).show()
+            calculateTip()
         }
+    }
+
+    private fun calculateTip() {
+        var cost = binding.costOfService.text.toString().toDoubleOrNull()
+        if (cost == null) {
+            cost = 0.0
+        }
+
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_20 -> 0.20
+            R.id.option_18 -> 0.18
+            else -> 0.15
+        }
+
+        var tip = cost * tipPercentage
+        if (binding.roundUpSwitch.isChecked) {
+            tip = ceil(tip)
+        }
+
+        val tipString = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipAmount.text = getString(R.string.tip_amount, tipString)
     }
 }
